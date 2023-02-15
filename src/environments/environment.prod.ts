5ffd101d94f33d832bc8ff5041dbe13199917498
20092248 <brady91700@gmail.com>
2023-02-15 13:53:52 +0100

    6 files modified

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBG1JdaVSev7mgGNRHzInzQ1q1IqZ5LF04",
+    authDomain: "lagua-shikomori.firebaseapp.com",
+    projectId: "lagua-shikomori",
+    storageBucket: "lagua-shikomori.appspot.com",
+    messagingSenderId: "137499573111",
+    appId: "1:137499573111:web:670bc2f91420e63f807f72",
+    measurementId: "G-56QVYPSYSH"
+  }
 };
