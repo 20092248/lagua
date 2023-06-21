652e1f012d630f9b72c06fd61836941f1d57bc21
Brady Abderemane <brady91700@gmail.com>
2023-06-21 13:29:52 +0200

    maj alphabet component

--- src/app/lessons/alphabet/alphabet.page.ts
+++ src/app/lessons/alphabet/alphabet.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthentificationService } from 'src/app/services/authentification.service';
+import { LessonService } from 'src/app/services/lesson.service';
 
 @Component({
   selector: 'app-alphabet',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlphabetPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, private authentificationService: AuthentificationService, private lessonService: LessonService) { }
 
   ngOnInit() {
+  }
+
+  saveLesson() {
+    if(this.authentificationService.user.uid){
+      this.authentificationService.updateLesson(this.authentificationService.user.lesson, 'users', this.authentificationService.user.uid).then(()=>{
+        this.router.navigate(['/tabs/lessons']);
+      });
+    } else {
+      console.error('Impossible de sauvegarder sans l\'identifiant de l\'utilisateur');
+    }
   }
 
 }
