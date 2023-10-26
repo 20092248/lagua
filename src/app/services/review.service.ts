877858d8c26cabcd24b77bf5f4fc8e8f6d2c1b3c
Brady Abderemane <brady91700@gmail.com>
2023-10-26 21:51:39 +0200

    MAJ ecran cours

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -102,9 +102,14 @@ export class ReviewService {
     const sourceQuestion = await getDoc(doc(getFirestore(), collectionSrc, sourceCategory + '_' + sourceLesson + '_' + sourceOrder));
     const questionRef = doc(getFirestore(), collectionSrc, category + '_' + lesson + '_' + order);
     const question = await getDoc(questionRef);
-    const qcm = question.data();
+    const sourceData = sourceQuestion.data() as any;
+    const data = question.data() as any;
+    console.log('source', sourceData.qcm.questions);
+    console.log('destination', data.qcm.questions);
+    const merge = data.qcm.questions.concat(sourceData.qcm.questions);
+    console.log('ajout', merge);
     await updateDoc(questionRef, {
-      qcm: qcm
+      'qcm.questions': merge
     });
   }
 
