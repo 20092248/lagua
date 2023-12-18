90cae1f335c587df5bc73ef69adf66254eeaafbf
Brady Abderemane <brady91700@gmail.com>
2023-12-18 14:47:21 +0100

    password smtp js

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,45 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CONSTANTS } from '../utils/constants';
-import { AlertService } from './alert.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import * as mail from '@sendgrid/mail';
+import '../utils/smtp.js';
+declare let Email: any;
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailService {
 
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + CONSTANTS.EMAIL_API_KEY
-    })
-  };
-
-  constructor(private http: HttpClient, private alertService: AlertService) { }
+  constructor() { }
 
   sendEmail(to: string, subject: string, text: string) {
 
-    // this.http.post(CONSTANTS.URL_SEND_GRID, {
-    //   personalizations: [{ to: [{ email: to }] }],
-    //   from: { from: CONSTANTS.TEAM_LAGUA_EMAIL },
-    //   subject: subject,
-    //   content: [{ type: 'text/plain', value: text }],
-    // }, this.httpOptions).subscribe(() => {
-    //   this.alertService.presentToast('Votre message a été envoyé.', 3000, 'success');
-    // });
-
-
-    mail.setApiKey(CONSTANTS.EMAIL_API_KEY);
-    mail.send({
-      to: to,
-      from: CONSTANTS.TEAM_LAGUA_EMAIL,
-      subject: subject,
-      text: text,
-      html: '<strong>' + text + '</strong>',
-    }).then(res => {
-      console.log(res);
-    });
   }
 
 }
+
+// 6B171CB1BFE2E2D2F144552FC7A773B4616D
