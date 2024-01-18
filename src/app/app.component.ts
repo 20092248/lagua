ff7c3c969760d342d966e7c133f7442d9a0e3c0d
Brady Abderemane <brady91700@gmail.com>
2024-01-18 22:20:50 +0100

    Ajout module splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 import { FacebookLogin } from '@capacitor-community/facebook-login';
 import { NotificationsService } from './services/notification.service';
+import { AlertService } from './services/alert.service';
+import { LoadingService } from './services/loading.service';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +14,22 @@ import { NotificationsService } from './services/notification.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private themeService: ThemeService, private platform: Platform, private pushNotificationsService: NotificationsService) {
+  constructor(private themeService: ThemeService, private platform: Platform, private pushNotificationsService: NotificationsService, private loadingService: LoadingService) {
+    
     const value = localStorage.getItem('selected-app-theme');
     this.themeService.setAppTheme(value ? value : 'sunny');
     this.initializeApp();
   }
 
   initializeApp() {
+    this.loadingService.present('Chargement en cours...');
     this.pushNotificationsService.initPush();
-    this.platform.ready().then(() => {
+    this.platform.ready().then(async() => {
+      if(this.platform.is('capacitor')){
+        await SplashScreen.hide();
+        await SplashScreen.show();
+      }
+      this.loadingService.dismiss();
       GoogleAuth.initialize();
       FacebookLogin.initialize({ 
         appId: '771703417822238' 
