380462fac73e81c8f195c53631022540d0e4ec39
Brady Abderemane <brady91700@gmail.com>
2024-01-21 22:20:01 +0100

    MAJ ecran detail

--- src/app/topics/detail/detail.module.ts
+++ src/app/topics/detail/detail.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -15,6 +15,7 @@ import { DetailPage } from './detail.page';
     IonicModule,
     DetailPageRoutingModule
   ],
-  declarations: [DetailPage]
+  declarations: [DetailPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DetailPageModule {}
