2d5cf3a216decbd49646dbec252e888296d65747
Brady Abderemane <brady91700@gmail.com>
2024-07-05 17:18:23 +0200

    MAJ écran add-word

--- src/app/dictionary/add-word/add-word.page.ts
+++ src/app/dictionary/add-word/add-word.page.ts
@@ -10,7 +10,7 @@ export class AddWordPage implements OnInit {
 
   category: string = 'FR';
   segments: string[] = ['FR', 'KM'];
-  wordToUpdate: any = {text: '', pluralText: '', translate:'', description: '', examples: [], siblings: []};
+  wordToUpdate: any = {text: '', pluralText: '', translate:'', description: '', examples: [{text: '', dialect: '', translate:''}], siblings: [{symbol: '', text: '', dialect: '', translate:''}]};
 
   constructor() { }
 
